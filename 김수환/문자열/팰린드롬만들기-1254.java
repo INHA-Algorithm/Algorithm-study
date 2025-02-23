1191ad914d3eb9baa2ce59dc8555ab97fe33c217
김수환 <101265039+genius00hwan@users.noreply.github.com>
2025-02-23 15:44:53 +0900

    [김수환-4주차 알고리즘 스터디]  (#26)
    * [Baekjoon-5972] 택배배송
    
    * 4주차

---
+++ 김수환/문자열/팰린드롬만들기-1254.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static BufferedReader br;
+    public static String str;
+    public static String rev;
+    public static int start;
+    public static boolean flag;
+
+    public static void init() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+        rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        start = 0;
+    }
+
+    public static void solve() {
+        while (start < str.length() - 1) {
+            for (int i = start; i < str.length() - 1; i++) {
+                if (str.charAt(i) != rev.charAt(i - start)) {
+                    flag = false;
+                    start++;
+                    break;
+                }
+                flag = true;
+            }
+            if (flag) {
+                break;
+            }
+        }
+        System.out.println(start + str.length());
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+        solve();
+    }
+}
